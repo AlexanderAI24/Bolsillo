f461a8b34006aa0ac78a0df953c4ab2832754fb7
AlexanderAI24 <alexhoyosgonzalez@outlook.com>
2026-08-22 12:56:34 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bolsillo-v8';
+const CACHE='bolsillo-v9';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
